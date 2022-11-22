245418b1896962fac763f4fb19890e66d85ce7b7
Aleksandra Prognimak <aleksandra.prognimak@gmail.com>
2022-11-22 20:20:30 +0200

    add linters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
@@ -8,12 +9,12 @@ import { App } from './App';
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<HomePage />} />
-          <Route path="home" element={<Navigate to="/" replace />} />
-          <Route path="add" element={<AddPage />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Route>
-      </Routes>
-  </BrowserRouter>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="add" element={<AddPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
 );
