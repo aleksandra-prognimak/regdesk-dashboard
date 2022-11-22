4c9eca82ad78b1aaae85dd47b83cd2d830575671
Aleksandra Prognimak <aleksandra.prognimak@gmail.com>
2022-11-22 18:46:25 +0200

    add routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { AddPage } from './pages/AddPage';
+import { PageNotFound } from './pages/PageNotFound';
+import { App } from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="home" element={<Navigate to="/" replace />} />
+          <Route path="add" element={<AddPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/AddPage/AddPage.jsx
@@ -0,0 +1,1 @@
+export const AddPage = () => <h1>Add Page</h1>;
